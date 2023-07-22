2df00b2c0bf2d6de38bbd543122ff5ab5c724747
LoFor1t <vgresnyh@gmail.com>
2023-07-22 13:17:21 +0200

    Create get request for getting exchange from one currency to other.

---
+++ src/main/java/com/curencyexchange/currencyexchange/Exchange.java
@@ -0,0 +1,19 @@
+package com.curencyexchange.currencyexchange;
+
+import java.math.BigDecimal;
+
+public class Exchange {
+    public Currency baseCurrency;
+    public Currency targetCurrency;
+    public BigDecimal rate;
+    public String amount;
+    public BigDecimal convertedAmount;
+
+    public Exchange(Currency baseCurrency, Currency targetCurrency, BigDecimal rate, String amount, BigDecimal convertedAmount) {
+        this.baseCurrency = baseCurrency;
+        this.targetCurrency = targetCurrency;
+        this.rate = rate;
+        this.amount = amount;
+        this.convertedAmount = convertedAmount;
+    }
+}
---
+++ src/main/java/com/curencyexchange/currencyexchange/servlets/ExchangeServlet.java
@@ -0,0 +1,65 @@
+package com.curencyexchange.currencyexchange.servlets;
+
+import com.curencyexchange.currencyexchange.CurrencyModel;
+import com.curencyexchange.currencyexchange.Exchange;
+import com.curencyexchange.currencyexchange.ExchangeRate;
+import com.curencyexchange.currencyexchange.ExchangeRateModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.sql.SQLException;
+
+@WebServlet("/exchange")
+public class ExchangeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ExchangeRate exchangeRate;
+        BigDecimal rate;
+
+        String from = req.getParameter("from");
+        String to = req.getParameter("to");
+        String amount = req.getParameter("amount");
+
+        if (from == null || to == null || amount == null) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Отсутствует нужно поле формы");
+            return;
+        }
+
+        try {
+            exchangeRate = ExchangeRateModel.getExchangeRateByCode(from, to);
+            if (exchangeRate != null) {
+                rate = exchangeRate.rate;
+            } else {
+                exchangeRate = ExchangeRateModel.getExchangeRateByCode(to, from);
+                if (exchangeRate != null) {
+                    rate = BigDecimal.valueOf(1/exchangeRate.rate.doubleValue());
+                } else {
+                    ExchangeRate fromExchangeRate = ExchangeRateModel.getExchangeRateByCode("USD", from);
+                    ExchangeRate toExchangeRate = ExchangeRateModel.getExchangeRateByCode("USD", to);
+                    if (fromExchangeRate != null && toExchangeRate != null) {
+                        rate = BigDecimal.valueOf(toExchangeRate.rate.doubleValue() / fromExchangeRate.rate.doubleValue());
+                    } else {
+                        resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Такой валютной пары не существует.");
+                        return;
+                    }
+                }
+            }
+            new ObjectMapper().writeValue(resp.getWriter(), new Exchange(CurrencyModel.getCurrencyByCode(from),
+                    CurrencyModel.getCurrencyByCode(to),
+                    rate,
+                    amount,
+                    rate.multiply(BigDecimal.valueOf(Double.parseDouble(amount)))));
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getSQLState());
+        }
+    }
+}
